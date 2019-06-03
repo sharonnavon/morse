aabef585ffe9c8da86c5e3185cb4850250f73bd2
sharonnavon <sharon.nik@gmail.com>
2019-06-03 14:39:14 +0300

    Adding all files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,12 @@
 module "us-east-1" {
-  source = "./morse"
+  source = "./modules/morse"
   region = "us-east-1"
   ami_ubuntu = "ami-0a313d6098716f372"
   key_name = "aws_ec2"
 }
 
 module "us-west-1" {
-  source = "./morse"
+  source = "./modules/morse"
   region = "us-west-1"
   ami_ubuntu = "ami-06397100adf427136"
   key_name = "aws_ec2_us-west-1"
